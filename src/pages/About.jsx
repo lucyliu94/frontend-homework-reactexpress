e2b117568e7c82b826cb571b5e969ee02219641f
Lucy Liu <lucy.liu94@gmail.com>
2021-11-09 14:47:39 -0500

    added built script

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,33 +1,39 @@
 import { useState, useEffect } from "react";
 
-function About(props) {
-  // create state to hold about data
+
+const About = props => {
+
+  // create state to hold "about" data
   const [about, setAbout] = useState(null);
 
-  // create function to make api call
+  // create function to make API call
   const getAboutData = async () => {
-    // make api call and get response
     const response = await fetch(props.URL + "about");
-    // turn response into javascript object
     const data = await response.json();
-    // set the about state to the data
+    console.log(data);
     setAbout(data);
-  };
+  }
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  // make an initial call for the data inside a useEffect hook, so it only happens once here
   useEffect(() => getAboutData(), []);
 
   // define a function that will return the JSX needed once we get the data
-  const loaded = () => (
-    <div>
-      <h2>{about.name}</h2>
-      <h3>{about.email}</h3>
-      <p>{about.bio}</p>
-    </div>
-  );
+  const loaded = () => {
+    return (
+      <div>
+        <h2>{about.name}</h2>
+        <h3>{about.email}</h3>
+        <p>{about.bio}</p>
+      </div>
+    )
+  };
 
-  // if data arrives return the result of loaded, if not, an h1 that says loading
-  return about ? loaded() : <h1>Loading...</h1>;
+  // if data arrives return the result of loaded(). If not, return an h1 that says "Loading..."
+  return (
+      about ? loaded() : <h1>Loading...</h1>
+    );
 }
+
+
 
 export default About;
